7a857f7d76334d814246c569dc345fce926d4411
dogsub <ehdtjq0909@gmail.com>
2025-01-26 14:45:15 +0900

    [feat] #53 마이페이지 청첩장 목록 API 구현 및 자동 정렬, 자동 삭제 로직 구현

--- src/main/java/com/wedit/weditapp/domain/member/controller/MemberController.java
+++ src/main/java/com/wedit/weditapp/domain/member/controller/MemberController.java
@@ -1,13 +1,18 @@
 package com.wedit.weditapp.domain.member.controller;
 
-import com.wedit.weditapp.domain.member.dto.response.MemberResponseDto;
-import com.wedit.weditapp.domain.member.service.MemberService;
+import com.wedit.weditapp.domain.invitation.dto.response.InvitationResponseDto;
+import com.wedit.weditapp.domain.invitation.service.InvitationService;
+import com.wedit.weditapp.global.error.ErrorCode;
+import com.wedit.weditapp.global.error.exception.CommonException;
 import com.wedit.weditapp.global.response.GlobalResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,32 +22,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberController {
 
-    private final MemberService memberService;
+    private final InvitationService invitationService;
 
-    // 모든 회원 조회 API
-    @Operation(summary = "모든 회원 조회", description = "등록된 모든 회원의 정보를 조회합니다.")
+    // 마이페이지 - 해당 회원의 청첩장 목록 불러오기 API
+    @Operation(summary = "청첩장 목록 조회", description = "해당 회원이 제작한 모든 청첩장들의 정보를 조회합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "조회 성공"),
-            @ApiResponse(responseCode = "404", description = "회원 목록을 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "404", description = "유저를 찾을 수 없습니다."),
             @ApiResponse(responseCode = "500", description = "서버 에러")
     })
-    @GetMapping
-    public ResponseEntity<GlobalResponseDto<List<MemberResponseDto>>> findAllMembers() {
-        List<MemberResponseDto> memberResponses = memberService.findAllMembers();
-        return ResponseEntity.ok(GlobalResponseDto.success(memberResponses));
-    }
-
-    // 단일 회원 조회 API
-    @Operation(summary = "단일 회원 조회", description = "해당 id를 가진 회원의 정보를 조회합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "조회 성공"),
-            @ApiResponse(responseCode = "404", description = "해당 회원을 찾을 수 없습니다."),
-            @ApiResponse(responseCode = "500", description = "서버 에러")
-    })
-    @GetMapping("/{userId}")
-    public ResponseEntity<GlobalResponseDto<MemberResponseDto>> findMember(
-            @PathVariable Long userId) {
-        MemberResponseDto memberResponse = memberService.findMember(userId);
-        return ResponseEntity.ok(GlobalResponseDto.success(memberResponse));
+    @GetMapping("/mypage")
+    public ResponseEntity<GlobalResponseDto<List<InvitationResponseDto>>> getMemberInvitations(
+            @AuthenticationPrincipal UserDetails userDetails) {
+        List<InvitationResponseDto> invitations = invitationService.getMemberInvitations(userDetails);
+        return ResponseEntity.ok(GlobalResponseDto.success(invitations));
     }
 }
